2962d98c4a26c84bfacece1ccb2c5c924ebf75a7
Aymen Benmohammed <100866236+aymen723@users.noreply.github.com>
2023-12-06 00:35:24 +0100

    its used now

--- main.c
+++ main.c
@@ -8,7 +8,11 @@
  */
 int main(void)
 {
+    int len;
+    int len2 = 0;
 
-    int len = _printf("Let's try to printf a simple sentence.\n");
+    len = _printf("Let's try to printf a simple sentence.\n");
+    printf("Length:[%d, %i]\n", len2, len2);
+
     return (0);
 }
